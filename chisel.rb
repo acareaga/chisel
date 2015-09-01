6a491755293e39176db5a0ed66cb21a3f4563106
acareaga <aaroncareaga@gmail.com>
2015-09-01 17:29:20 -0600

    Building out tests for headers.

--- chisel.rb
+++ chisel.rb
@@ -1,8 +1,27 @@
 require 'pry'
 
 class Header
-  def initialize(word)
-    @word = word.downcase
+
+  def initialize(string)
+    true
+  end
+
+  def h1(string)
+    ending = " </h1>"
+    @string = string.gsub!("##", "<h1>")
+    @string + ending
+  end
+
+  def h2(string)
+    ending = " </h2>"
+    @string = string.gsub!("##", "<h2>")
+    @string + ending
+  end
+
+  def h3(string)
+    ending = " </h3>"
+    @string = string.gsub!("##", "<h3>")
+    @string + ending
   end
 
   # def check_rotation?(check_word)
@@ -13,3 +32,5 @@ class Header
   #   end
   # end
 end
+
+# puts "Converted my_input.markdown (6 lines) to my_output.html (8 lines)"
--- chisel_test.rb
+++ chisel_test.rb
@@ -7,6 +7,21 @@ class HeaderTest < Minitest::Test
     assert Header.new("apple")
   end
 
+  def test_h1(string)
+  ending = " </h1>"
+  Header.new(string)
+  end
+
+  def test_h2(string)
+  ending = " </h1>"
+  Header.new(string)
+  end
+
+  def test_h3(string)
+  ending = " </h1>"
+  Header.new(string)
+  end
+
   # def test_apple_is_a_rotation_of_apple
   #   apple = Header.new("Apple")
   #
